fd6ca28f42a99eb692654e09b9e6c7ab23fd18db
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-09-26 14:16:55 +0330

    Updates

--- src/Skeleton.jsx
+++
@@ -1,9 +0,0 @@
-export default function Skeleton({ lines = 1, width = "100%" }) {
-	return (
-		<div className="skeleton-container">
-			{Array.from({ length: lines }, (_, i) => (
-				<div key={i + 1} style={{ width }} className="skeleton"></div>
-			))}
-		</div>
-	);
-}
